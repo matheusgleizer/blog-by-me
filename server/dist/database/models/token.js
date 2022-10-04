3b64d16ec9ce96e2ac14b799be07d0b551a03b45
matheusgleizer <matheus@quickcommerce.org>
2022-10-04 02:09:08 +0200

    removes dist folder

--- server/dist/database/models/token.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const tokenSchema = new mongoose_1.Schema({
-    tags: [{ type: String }],
-});
-const Token = (0, mongoose_1.model)('x-access-token', tokenSchema);
-exports.default = Token;
